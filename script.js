e8b1cc9122233ddeb8b27425f65f0171ecfa1c53
bilal <bilal0558@gmail.com>
2021-08-04 13:22:44 +0530

    First commit to upload files.

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggle(){
+    document.querySelector(".sun-logo").classList.toggle("animate-sun");
+    document.querySelector(".moon-logo").classList.toggle("animate-moon");
+    document.querySelector("body").classList.toggle("dark");
+}
